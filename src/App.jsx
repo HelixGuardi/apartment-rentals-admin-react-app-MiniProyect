01b0b10eef34b93a87ae67db96e0bc113ce7cb07
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2024-12-06 16:41:26 +0100

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import MainSection from './components/MainSection'
-import Footer from './components/Footer'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import MainSection from "./components/MainSection";
+import Footer from "./components/Footer";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       <div>
         <Navbar />
-        <MainSection />
+        <Routes>
+          <Route path="/" element={ <MainSection /> }/>
+        </Routes>
         <Footer />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -5,14 +5,15 @@ function MainSection() {
   const [everyCard, setEveryCard] = useState(data.results);
 
   return (
+    <div className="main-section-container"> 
       <div>
         {everyCard.map((eachCard) => {
           return (
-            <li key={eachCard.id}>{eachCard.id}</li> 
-            /* cambiarlo por la estructura que queremos para el listado */
+            <li key={eachCard.id}>{eachCard.name}</li> 
           )
         })}
       </div>
+    </div>
   );
 }
 export default MainSection;
