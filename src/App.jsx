f85cc1b569e5ca6f5821385ea53fb58fd1f3e4ca
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2024-12-08 15:28:11 +0100

    routes organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import "./App.css";
 import Navbar from "./components/Navbar";
-import MainSection from "./components/MainSection";
 import Footer from "./components/Footer";
 import { Routes, Route, Link } from "react-router-dom";
+import RentCard  from "./components/RentCard"
 import Favorites from "./pages/Favorites"
 import SaberMas from "./pages/SaberMas";
 import Profile from "./pages/Profile";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -14,10 +15,13 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={ <MainSection /> } />
+          <Route path="/" element={ <RentCard />} />
           <Route path="/Favorites" element={ <Favorites /> } />
           <Route path="/SaberMas" element={ <SaberMas /> } /> {/* Hacer con que una vez se haga click en "Saber más" que salga en la URL el nombre del card al que se ha hecho click en el botón */}
           <Route path="/Profile" element={ <Profile /> } />
+          
+          <Route path="*" element={  <NotFound /> }/>
+
         </Routes>
         <Footer />
       </div>
